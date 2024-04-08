1c8cda99f9d805b66ffe3ea44cfdc46d6d0e0d1e
Martin Priebe <martin_priebe@hotmail.com>
2024-04-08 22:37:09 +0200

    fix error message

--- gatekeeper.js
+++ gatekeeper.js
@@ -6,7 +6,7 @@ const fileName_Image = process.env.GOOGLE_BUCKET_IMAGE_FILENAME;
 const { createImageWithText } = require('./image');
 
 if (!bucketName || !fileName_Gatekeeper || !fileName_Image) {
-    console.error('Error: The environment variables GOOGLE_BUCKET_NAME, GOOGLE_BUCKET_IMAGE_FILENAME and GOOGLE_BUCKET_FILENAME must be set.');
+    console.error('Error: The environment variables GOOGLE_BUCKET_NAME, GOOGLE_BUCKET_IMAGE_FILENAME and GOOGLE_BUCKET_GATEKEEPER_FILENAME must be set.');
     process.exit(1); // Exit the application if variables are not set
 }
 
