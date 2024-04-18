ee2fe1d539ae68755f5a6d0c572c5e13f2928136
Martin <martin_priebe@hotmail.com>
2024-04-18 10:01:34 +0200

    add vertex api for testing with json output

--- api/processor/processor.js
+++ api/processor/processor.js
@@ -15,7 +15,31 @@ app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 
+
 // Define the endpoint to handle POST requests
+app.post('/api/vertex', async (req, res) => {
+    try {
+        const { scenario, camera, lens, mail } = req.body;
+
+        if (!scenario || !camera) {
+            return res.status(400).json({
+                success: false,
+                message: "Missing required parameters. Please ensure 'scenario' and 'camera', are provided. 'lens' and 'mail' are optional."
+            });
+        }
+
+        await processData(camera, scenario, lens, undefined, res);
+
+    } catch (error) {
+        // Log the error for debugging purposes
+        console.error("Error during prediction:", error);
+
+        // Send back an error response
+        res.status(500).json({ success: false, message: "Internal server error." });
+    }
+});
+
+// Define the endpoint to handle GET requests
 app.get('/api/process', async (req, res) => {
 
     let fileName = "";
@@ -37,61 +61,8 @@ app.get('/api/process', async (req, res) => {
         mail = result.data.mail;
         fileName = result.fileName;
 
-        // interact with Vertex AI
-        let cameraPrompt1 = ""
-        let cameraPrompt2 = ""
+        await processData(camera, scenario, lens, mail, res);
 
-        //TODO: Currently ignored by the vertex ai api
-        //const outputFormatPrompt = "The output format should be a point bullet point list in html."
-        const outputFormatPrompt = "";
-
-        // 1) ask for camera settings as bullet point list 
-        cameraPrompt1 = "give me 7 camera setting with explanation when i want to photograph " + scenario + " using a " + camera + (lens ? " and lens " + lens : + ".");
-
-        // // 2) ask for composition tips as bullet point list 
-        cameraPrompt2 = "give me 7 compositions with explanation when i want to photograph " + scenario + " using a " + camera + (lens ? " and lens " + lens : ".");
-        const returned = await callVertexAIService(cameraPrompt1 + cameraPrompt2);
-        const resultSettingsAndComposition = extractTextFromResponse(returned);
-
-        // // 3) ask for more creative settings as bullet point list 
-        cameraPrompt1 = "give me 7 extraordinary and creative camera setting with explanation when i want to photograph " + scenario + " using a " + camera + (lens ? " and lens " + lens : ". It can be creative and use of extra equipment.");
-
-        // // 4) ask for more creative composition tips with equipment as bullet point list 
-        cameraPrompt2 = "give me 7 extraordinary compositions with extra equipment and creative camera settings with explanation hen i want to photograph " + scenario + " using a " + camera + (lens ? " and lens " + lens : ". Use different positions and angles.");
-        const returnedCreative = await callVertexAIService(cameraPrompt1 + cameraPrompt2);
-        const resultCreativeSettingsAndComposition = extractTextFromResponse(returnedCreative);
-
-        // // 5) ask for things to avoid
-        cameraPrompt1 = "give me 7 things to avoid and common mistakes with camera " + camera + (lens ? " and lens " + lens : ". I want to photograph the scenario " + scenario);
-        const returnedAvoid = await callVertexAIService(cameraPrompt1);
-        const resultAvoid = extractTextFromResponse(returnedAvoid);
-
-        // // 6) create an image with 1) and 2)
-        //TODO: implement image generation with vertex ai, aws or openai
-
-        // // 7) create an image with 3) and 4)
-        //TODO: implement image generation with vertex ai, aws or openai
-
-        if (mail) {
-
-            // 8) create pdf
-            const pdfBase64 = await generatePDF(scenario, resultSettingsAndComposition, resultCreativeSettingsAndComposition, resultAvoid);
-
-            //DEBUG 
-            //const content = await generateHTML(scenario, resultSettings, resultComposition, resultCreativeSettings, resultCreativeComposition, resultAvoid);
-
-            // send mail
-            await sendMail(mail, pdfBase64);
-
-            res.status(200).send({ message: "process finished" });
-        }
-        else {
-            // create json
-            const json = await generateJSON(scenario, resultSettingsAndComposition, resultCreativeSettingsAndComposition, resultAvoid);
-
-            // return json
-            res.status(200).json({ success: true, data: json });
-        }
     } catch (error) {
         // Log the error for debugging purposes
         console.error("Error during prediction:", error);
@@ -105,6 +76,64 @@ app.get('/api/process', async (req, res) => {
         res.status(500).json({ success: false, message: "Internal server error." });
     }
 });
+
+
+async function processData(camera, scenario, lens, mail, res) {
+    let cameraPrompt1 = ""
+    let cameraPrompt2 = ""
+
+    //TODO: Currently ignored by the vertex ai api
+    //const outputFormatPrompt = "The output format should be a point bullet point list in html."
+    const outputFormatPrompt = "";
+
+    // 1) ask for camera settings as bullet point list 
+    cameraPrompt1 = "give me 7 camera setting with explanation when i want to photograph " + scenario + " using a " + camera + (lens ? " and lens " + lens : + ".");
+
+    // // 2) ask for composition tips as bullet point list 
+    cameraPrompt2 = "give me 7 compositions with explanation when i want to photograph " + scenario + " using a " + camera + (lens ? " and lens " + lens : ".");
+    const returned = await callVertexAIService(cameraPrompt1 + cameraPrompt2);
+    const resultSettingsAndComposition = extractTextFromResponse(returned);
+
+    // // 3) ask for more creative settings as bullet point list 
+    cameraPrompt1 = "give me 7 extraordinary and creative camera setting with explanation when i want to photograph " + scenario + " using a " + camera + (lens ? " and lens " + lens : ". It can be creative and use of extra equipment.");
+
+    // // 4) ask for more creative composition tips with equipment as bullet point list 
+    cameraPrompt2 = "give me 7 extraordinary compositions with extra equipment and creative camera settings with explanation hen i want to photograph " + scenario + " using a " + camera + (lens ? " and lens " + lens : ". Use different positions and angles.");
+    const returnedCreative = await callVertexAIService(cameraPrompt1 + cameraPrompt2);
+    const resultCreativeSettingsAndComposition = extractTextFromResponse(returnedCreative);
+
+    // // 5) ask for things to avoid
+    cameraPrompt1 = "give me 7 things to avoid and common mistakes with camera " + camera + (lens ? " and lens " + lens : ". I want to photograph the scenario " + scenario);
+    const returnedAvoid = await callVertexAIService(cameraPrompt1);
+    const resultAvoid = extractTextFromResponse(returnedAvoid);
+
+    // // 6) create an image with 1) and 2)
+    //TODO: implement image generation with vertex ai, aws or openai
+
+    // // 7) create an image with 3) and 4)
+    //TODO: implement image generation with vertex ai, aws or openai
+
+    if (mail) {
+
+        // 8) create pdf
+        const pdfBase64 = await generatePDF(scenario, resultSettingsAndComposition, resultCreativeSettingsAndComposition, resultAvoid);
+
+        //DEBUG 
+        //const content = await generateHTML(scenario, resultSettings, resultComposition, resultCreativeSettings, resultCreativeComposition, resultAvoid);
+
+        // send mail
+        await sendMail(mail, pdfBase64);
+
+        res.status(200).send({ message: "process finished" });
+    }
+    else {
+        // create json
+        const json = await generateJSON(scenario, resultSettingsAndComposition, resultCreativeSettingsAndComposition, resultAvoid);
+
+        // return json
+        res.status(200).json({ success: true, data: json });
+    }
+}
 
 function extractTextFromResponse(response) {
     const result = response.candidates?.[0]?.content?.parts?.[0]?.text ?? "-";
